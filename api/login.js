1dc5f33f6308c54f8882d3689ec9ed6c87c14412
shn999 <1260819292@qq.com>
2017-11-22 17:04:42 +0800

    t

--- api/login.js
+++ api/login.js
@@ -5,22 +5,23 @@ router.route('/login')
     .get((req,res,next) => {
         const username = "admin";
         const password = "123456";
-        if (req.query.username === username && req.query.password)
-        console.log(req.query);
-        res.status(200).json({
-            status: 1,
-            data:{
-                username:username
-            },
-            message: '登录成功'
-        })
-}) else {
-    res.status(200).json({
-        status: 0,
-        data:{
-            username: req.query.username
-        },
-        message: '登陆失败'
     })
-
+        if (req.query.username === username && req.query.password){
+            console.log(req.query);
+            res.status(200).json({
+                status: 1,
+                data:{
+                    username:username
+                },
+                message: '登录成功'
+            })
+        } else {
+            res.status(200).json({
+                status: 0,
+                data: {
+                    username: req.query.username
+                },
+                message: '登陆失败'
+            })
+        }
 module.exports = router;
